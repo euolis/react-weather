1fe879f1c96be54ce9f0e9064849f0f0d7c3e51b
Michael James <m.james.cs@outlook.com>
2018-05-13 10:58:24 -0500

    add open weather api

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=6c85d9845e159f27ab006c68afff1235&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${ OPEN_WEATHER_MAP_URL }&q=${ encodedLocation }`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
